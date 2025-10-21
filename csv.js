70ca5cb7a0178776ef63b158b84405ffdd34de58
nato-git <s.f.h.o1219@gmail.com>
2025-10-21 17:10:36 +0900

    javascriptとcsvファイルを変更

---
+++ csv.js
@@ -0,0 +1,23 @@
+async function csvFile() {
+  const csvfile = await fetch('kanji.csv');
+  const csvtext = await csvfile.text();
+  const rows = csvtext.split('\n');
+  const data = rows.map((row) => {
+    const columns = row.split(',');
+    return {
+      教科: columns[0],
+      ID: columns[1],
+      内容: columns[2],
+      元の行番号: columns[3],
+    };
+  });
+  for (let i = 0; i < data.length; i++) {
+    document.getElementById('mains').innerHTML += `
+      <div>
+        <p>内容: ${data[i].内容}</p>
+      </div>
+    `;
+  }
+}
+
+csvFile();
