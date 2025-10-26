901646f460a429f096d8ba1a0a6d294e5a048e5c
nato-git <s.f.h.o1219@gmail.com>
2025-10-26 16:45:32 +0900

    uooooooo

--- csv.js
+++ csv.js
@@ -83,7 +83,7 @@ async function csvFile() {
 // グローバルスコープに配置された kanjiButton 関数
 // クリック時に実行され、グローバルの kanjiData にアクセスする
 function kanjiButton(i) {
-  const kanjiHTML = `<div color=#ff6efa>
+  const kanjiHTML = `<div class="kanji-detail">
     <h2>漢字：${kanjiData[i].内容}</h2>
     <p>音読み：${kanjiData[i].音読み}</p>
     <p>訓読み：${kanjiData[i].訓読み}</p>
